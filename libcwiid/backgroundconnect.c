adde29ac0536348481885bc7ac4e643217832340
Pekka Vanhoja <pevanhoj@pevanhoj-desktop.(none)>
2010-01-10 00:41:43 +0200

    Added marshal generation to build, device discovery started.

--- libcwiid/backgroundconnect.c
+++ libcwiid/backgroundconnect.c
@@ -5,6 +5,11 @@
 #include <dbus/dbus.h>
 #include <dbus/dbus-glib.h>
 #include "cwiid_internal.h"
+#include "marshal.h"
+
+#define OSSOLOG_COMPILE
+#define OSSOLOG_STDOUT
+#include <osso-log.h>
 
 // BlueZ DBus API strings
 #define BLUEZ_NAME "org.bluez"
@@ -36,9 +41,56 @@ static void clean_data(BackgroundSearchData *data)
     }
 }
 
+static void device_found(DBusGProxy *adapter,
+                         const gchar *address,
+                         GHashTable *properties,
+                         gpointer user_data)
+{
+    ULOG_DEBUG_F("A Bluetooth device is found at: %s", address);
+}
+
 static gboolean start_wiimote_query(gpointer user_data)
 {
-    
+    GError *error = NULL;
+    BackgroundSearchData *data = (BackgroundSearchData *)user_data;
+
+    ULOG_DEBUG_F("Starting device discovery");
+
+    /* Start listening DeviceFound signals */
+    dbus_g_object_register_marshaller(marshal_VOID__STRING_POINTER,
+                                      G_TYPE_NONE,
+                                      G_TYPE_STRING,
+                                      G_TYPE_POINTER,
+                                      G_TYPE_INVALID);
+
+    dbus_g_proxy_add_signal(data->adapter_proxy,
+                            "DeviceFound",
+                            G_TYPE_STRING,
+                            dbus_g_type_get_map("GHashTable",
+                                                G_TYPE_STRING, G_TYPE_VALUE),
+                            G_TYPE_INVALID);
+    dbus_g_proxy_connect_signal(data->adapter_proxy,
+                                "DeviceFound",
+                                G_CALLBACK(device_found),
+                                data,
+                                NULL);
+
+    /* Start device discovery */
+    if (!dbus_g_proxy_call(data->adapter_proxy,
+                           "StartDiscovery",
+                           &error,
+                           G_TYPE_INVALID,
+                           G_TYPE_INVALID)) {
+        g_clear_error(&error);
+        clean_data(data);
+        g_free(data);
+        search_data = NULL;
+        /* TODO: Error handling */
+        ULOG_ERR_F("Error in starting discovery");
+        return FALSE;
+    }
+
+    return FALSE;
 }
 
 static gboolean check_adapter_powered(gpointer user_data)
@@ -51,11 +103,12 @@ static gboolean check_adapter_powered(gpointer user_data)
     value = g_hash_table_lookup(data->adapter_properties, "Powered");
     powered = value ? g_value_get_boolean(value) : FALSE;
     
-    g_print("default adapter: %s, adapter powered: %d\n", data->default_adapter, powered);
+    ULOG_DEBUG_F("default adapter: %s, adapter powered: %d",
+                 data->default_adapter, powered);
 
     /* Turn adapter on if it was turned off */
     if (value && !powered) {
-        GValue new_value = { 0 };
+        GValue new_value = { 0, };
         g_value_init(&new_value, G_TYPE_BOOLEAN);
         g_value_set_boolean(&new_value, TRUE);
         if (!dbus_g_proxy_call(data->adapter_proxy,
@@ -70,15 +123,16 @@ static gboolean check_adapter_powered(gpointer user_data)
             g_free(data);
             search_data = NULL;
             /* TODO: Error handling */
-            g_print("Error in setting adapter on\n");
+            ULOG_ERR_F("Error in setting adapter on\n");
             return FALSE;
         }
     }
 
-    clean_data(data);
-    g_free(data);
+    if (!g_idle_add(start_wiimote_query, data)) {
+        /* TODO: Error handling */
+    }
+
     return FALSE;
-    /* TODO: start searching for Wiimotes */
 }
 
 static gboolean get_adapter_properties(gpointer user_data)
@@ -99,8 +153,8 @@ static gboolean get_adapter_properties(gpointer user_data)
         clean_data(data);
         g_free(data);
         search_data = NULL;
-        /* TODO: Error handlind */
-        g_print("Error getting default adapter properties\n");
+        /* TODO: Error handling */
+        ULOG_ERR_F("Error getting default adapter properties");
         return FALSE;
     }
 
@@ -111,8 +165,18 @@ static gboolean get_adapter_properties(gpointer user_data)
     return FALSE;
 }
 
+static void adapter_property_changed(DBusGProxy *adapter,
+                                     const gchar *property,
+                                     GValue *value,
+                                     gpointer user_data)
+{
+    ULOG_DEBUG_F("Changed property: '%s'", property);
+}
+
 static gboolean get_default_adapter(gpointer user_data)
 {
+    ULOG_DEBUG_F();
+
     GError *error = NULL;
     BackgroundSearchData *data = (BackgroundSearchData *)user_data;
 
@@ -133,15 +197,38 @@ static gboolean get_default_adapter(gpointer user_data)
         g_free(data);
         search_data = NULL;
         /* TODO: Error handling */
-        g_print("Error getting default adapter path\n");
+        ULOG_ERR_F("Error getting default adapter path");
         return FALSE;
     }
 
     /* Get default adapter */
-    data->adapter_proxy = dbus_g_proxy_new_for_name(data->sys_dbus,
+    data->adapter_proxy = /*dbus_g_proxy_new_for_name(data->sys_dbus,
                                                     BLUEZ_NAME,
                                                     data->default_adapter,
-                                                    BLUEZ_ADAPTER_INTERFACE);
+                                                    BLUEZ_ADAPTER_INTERFACE);*/
+        dbus_g_proxy_new_from_proxy(data->manager_proxy,
+                                    BLUEZ_ADAPTER_INTERFACE,
+                                    data->default_adapter);
+
+
+    /* Start listening PropertyChanged signals from adapter */
+    dbus_g_object_register_marshaller(marshal_VOID__STRING_BOXED,
+                                      G_TYPE_NONE,
+                                      G_TYPE_STRING,
+                                      G_TYPE_VALUE,
+                                      G_TYPE_INVALID);
+
+    dbus_g_proxy_add_signal(data->adapter_proxy,
+                            "PropertyChanged",
+                            G_TYPE_STRING,
+                            G_TYPE_VALUE,
+                            G_TYPE_INVALID);
+
+    dbus_g_proxy_connect_signal(data->adapter_proxy,
+                                "PropertyChanged",
+                                G_CALLBACK(adapter_property_changed),
+                                data,
+                                NULL);
 
     if (!g_idle_add(get_adapter_properties, data)) {
         /* TODO: Error handling */
@@ -171,7 +258,7 @@ int cwiid_start_background_seach(/*cwiid_wiimote_connected_callback_t *callback*
     }
 
     data->sys_dbus = sys_dbus;
-    
+
     if (!g_idle_add(get_default_adapter, data)) {
         clean_data(data);
         g_free(data);
